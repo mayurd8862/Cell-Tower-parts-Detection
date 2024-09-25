d4a68e92e994806181800676bee1fd9632118292
Mayur Balu Dabade <mayurdabade1103@gmail.com>
2024-09-25 13:18:23 +0530

    updated codes

--- app.py
+++ app.py
@@ -5,28 +5,30 @@ import numpy as np
 from PIL import Image
 
 # Load the trained model
-model = YOLO('best.pt')  # Path to your trained model
+model = YOLO('./runs/detect/train/weights/last.pt')  # Path to your trained model
 
 # Streamlit app
-st.title('YOLO Object Detection')
+st.title('🗼 Mobile Tower Components Detection')
 
 st.write('Upload an image to perform object detection.')
 
 # Upload image
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-if uploaded_file is not None:
+btn = st.button("Analyze")
+if btn and uploaded_file is not None:
     # Read the image file
-    image = Image.open(uploaded_file).convert("RGB")
-    # Convert image to a format that OpenCV can handle
-    img_np = np.array(image)
-    img = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+    with st.spinner("Please wait analyzing Image"):
+        image = Image.open(uploaded_file).convert("RGB")
+        # Convert image to a format that OpenCV can handle
+        img_np = np.array(image)
+        img = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
 
-    # Run detection
-    results = model(img)
+        # Run detection
+        results = model(img)
 
-    # Draw bounding boxes on the image
-    annotated_img = results[0].plot()
-    annotated_img_rgb = cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB)
-    st.image(annotated_img_rgb, caption='Detected Image', use_column_width=True)
+        # Draw bounding boxes on the image
+        annotated_img = results[0].plot()
+        annotated_img_rgb = cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB)
+        st.image(annotated_img_rgb, caption='Detected Image', use_column_width=True)
 
--- test.py
+++ test.py
@@ -2,11 +2,11 @@ from ultralytics import YOLO
 import cv2
 
 # Load the trained model
-model = YOLO('best.pt')  # Path to your trained model
+model = YOLO('./runs/detect/train/weights/last.pt')  # Path to your trained model
 
 # Load an image
-img_path = 'data/train/images/6.jpg'  # Replace with your image path
-# img_path = 'c2.png' 
+img_path = 'image.png'  # Replace with your image path
+
 img = cv2.imread(img_path)
 
 # Run detection
